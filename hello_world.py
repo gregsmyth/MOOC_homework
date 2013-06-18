b54f85f2a4328b18c51e2cdff89fa9146619d36c
Greg Smyth <greg.smyth@gmail.com>
2013-06-18 21:53:33 +0100

    Exercises completed
    Homework for MOOC week 1 exercises 1.2-1.4

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+# Greg Smyth
+# hello_world.py
+#MOOC Week1
+#18th June 2013
+
+#prints "Hello, world!" to the screen
+print "Hello, world!"
---
+++ hw1.py
@@ -0,0 +1,61 @@
+# Name: Greg Smyth
+# Section: MOOC
+# Date:18th June 2013
+# hw1.py
+
+##### Template for Homework 1, exercises 1.2-1.5 ######
+
+print "********** Exercise 1.2 **********"
+
+# Do your work for Exercise 1.2 here
+
+print "  |  |  "
+print "--------"
+print "  |  |  "
+print "--------"
+print "  |  |  "
+
+
+print "********** Exercise 1.3 **********"
+
+# Do your work for Excercise 1.3 here. Hint - how many different
+# variables will you need?
+
+lines= "  |  |  "
+dashes= "--------"
+
+print lines
+print dashes
+print lines
+print dashes
+print lines
+
+
+print "********** Exercise 1.4 **********"
+print "********* Part II *************"
+
+a = 3*5/(2+3)
+print a
+
+b=(7+9)**0.5*2
+print b
+
+c=(4-7)**3
+print c
+
+d=(-19+100)**0.25
+print d
+
+e=6%4
+print e
+
+
+
+print "********* Part III *************"
+
+x = 3+7*25
+y= (3+7)*25
+
+print x
+print y
+
