b0e96afc2ec536fab7d0bc2d9dd162e0e70b8900
Greg Smyth <greg.smyth@gmail.com>
2013-06-28 21:15:27 +0100

    Exercise 1.5 from week 2 added

---
+++ wk2_names.py
@@ -0,0 +1,15 @@
+# Name: Greg Smyth
+# Homework MOOC Wk2
+# wk2_names.py
+# 28th June 2013
+
+print("MOOC Week 2 Homework")
+print("Exercise 1.5")
+first_name = raw_input("Enter your first name: ")
+last_name = raw_input("Enter your last name: ")
+print("Enter your date of birth:")
+dob_month = raw_input("Month? ")
+dob_day = raw_input("Day? ")
+dob_year = raw_input("Year? ")
+
+print first_name, last_name + " was born on", dob_month, dob_day + ",", dob_year
